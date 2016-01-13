db3f00efff1a1a11857622fb7cf66c93e942d2c9
ReturnZer0 <raofei23@gmail.com>
2016-01-13 22:10:39 +0800

    re module
    re模块的练习 匹配相关的连接

---
+++ re_module.py
@@ -0,0 +1,9 @@
+import re
+
+file = open(r'E:\大学课程相关\英语学习相关\voa英语\VOA慢速英语听力\VOA慢速英语听力2012年1—8月\2012年04月VOA慢速英语听力音频打包下载\15000-Websites-That-Spread-Terror-and-Hate.txt','r')
+
+for line in file:
+    object = re.match('^https?://',line)
+    if object:
+        print(line,end='')
+    #print(line,end='')
